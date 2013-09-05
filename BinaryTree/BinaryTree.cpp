c814a99c25840b9f578e9c0ad97bf78b0852f14a
yannxin <yannxin@qq.com>
2013-09-05 11:30:27 +0800

    test

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -5,22 +5,69 @@
 
 void InsertNode(BSTree &BST, int value)
 {
-	BSTree node = new struct BSTreeNode();
-	node->value = value;
-	node->count = 1;
-	node->left = node->right = NULL;
-
+/*
+ //insert node recursively.
 	if (BST == NULL)
+	{
+		BSTree node = new struct BSTreeNode();
+		node->value = value;
+		node->count = 1;
+		node->left = node->right = NULL;
 		BST = node;
+//		delete node;
+	}
 	else if (value < BST->value)
 		InsertNode(BST->left, value);
 	else if (value > BST->value)
 		InsertNode(BST->right, value);
 	else if (value == BST->value)
 		++BST->count;
+*/
+	// insert node circularly
+	BSTree node = new struct BSTreeNode();
+	node->value = value;
+	node->count = 1;
+	node->left = node->right = NULL;
+	if (BST == NULL)
+	{
+		BST = node;
+	}
+	else
+	{
+		BSTree p = BST;
+		while (p)
+		{
+			if (value == p->value)
+			{
+				p->count++;
+				break;
+			}
+			else if (value < p->value)
+			{
+				if (p->left)
+					p = p->left;
+				else
+				{
+					p->left = node;
+					break;
+				}
+
+			}
+			else if (value > p->value)
+			{
+				if (p->right)
+					p = p->right;
+				else
+				{
+					p->right = node;
+					break;
+				}
+			}
+		}
+	}
 }
 
-void DeleteNode(BSTreeNode *BSTree, int value)
+void DeleteNode(BSTree &BST, int value)
 {
 
 }
@@ -37,9 +84,19 @@ void BuildBSTree(BSTree &BST, int data[], int low, int high)
 	}
 }
 
-BSTree Search(BSTree &BST, int value)
+BSTree SearchBST(BSTree &BST, int value)
 {
-	return BST;
+	BSTree p = BST;
+	while (p)
+	{
+		if (value == p->value)
+			return p;
+		else if (value < p->value)
+			p = p->left;
+		else if (value > p->value)
+			p = p->right;
+	}
+	return NULL;
 }
 void PreOrderTraverse(BSTree BST)
 {
@@ -64,7 +121,6 @@ void MidOrderTraverse(BSTree BST)
 		{
 			printf("%d ", BST->value);
 		}
-		printf("%d ", BST->value);
 		MidOrderTraverse(BST->right);
 	}
 }
@@ -80,7 +136,6 @@ void PostOrderTraverse(BSTree BST)
 		{
 			printf("%d ", BST->value);
 		}
-		printf("%d ", BST->value);
 	}
 }
 
--- BinaryTree/BinaryTreeTest.cpp
+++ BinaryTree/BinaryTreeTest.cpp
@@ -6,12 +6,25 @@
 
 void test1()
 {
-	int data[] = {35, 6, 15, 65, 7, 3, 9, 26, 18, 36, 3, 46, 17, 43};
+	int data[] = {35, 6, 15, 65, 7, 17, 3, 9, 26, 18, 36, 3, 46, 17, 43, 3};
+//	int data[] = {3};
 	int size = sizeof(data)/sizeof(*data);
 //	printf("size = %d\n", size);
 	BSTree BST = NULL;
 	BuildBSTree(BST, data, 0, size - 1);
 	MidOrderTraverse(BST);
+	printf("\n");
+	PreOrderTraverse(BST);
+	printf("\n");
+	PostOrderTraverse(BST);
+	printf("\n");
+
+	BSTree node = SearchBST(BST, 18);
+	if (!node)
+		printf("Search failed.\n");
+	else
+		printf("Search success, the search value is %d.\n", node->value);
+
 	printf("\n");
 	
 }
